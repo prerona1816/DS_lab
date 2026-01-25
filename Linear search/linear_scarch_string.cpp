88d0227789dbd0ccf6d2bf4dc5a81f41e7457477
Umma Shahala Alam Prerona <prerona0186@gmail.com>
2026-01-25 13:47:02 +0600

    String in linear scarch

---
+++ Linear search/linear_scarch_string.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  string name = "Prerona" ;
+  char ch;
+  cin >> ch; 
+  int not_found = 1;
+  for(int i = 0 ; name[i]; i++)
+  {
+    if(name[i]==ch)
+    {
+        cout << ch << " " << "is founded" << endl;
+        not_found=0;
+        break;
+    }
+  }
+  if(not_found){
+    cout << "Not Found" << endl;
+  }
+    return 0;
+}
